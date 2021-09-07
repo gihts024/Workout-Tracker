c7076848d1f6092b74db42cce2b4a5b06aea096a
Alex Gihts <githenyalex@gmail.com>
2021-09-07 09:28:52 -0700

    test deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(compression());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gihts", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
